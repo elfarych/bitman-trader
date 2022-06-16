896ac726524c7829be8bc804248bbd6c7e0a56ce
Space Dev <devspace88@gmail.com>
2022-06-17 03:25:46 +0600

    v5

--- Node/index.js
+++ Node/index.js
@@ -3,8 +3,5 @@ const globals = require('./globals')
 
 
 globals.setGlobals()
-<<<<<<< HEAD
-trader.start().then(() => $logger(`Trader v2 started...`))
-=======
-trader.start().then(() => $logger(`Trader started v3...`))
->>>>>>> 3386074f10d162eeda1970bdd37aff190365fff6
+
+trader.start().then(() => $logger(`Trader started v4...`))
